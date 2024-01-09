b6db5291b5445ef685b04c09d891437365734a6e
Acors24 <329827@uwr.edu.pl>
2024-01-09 22:56:47 +0100

    add main js file for testing the db wrapper

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const pg = require("pg");
+const ProductRepository = require("./db/ProductRepository");
+
+(async function main() {
+    const repo = new ProductRepository();
+
+    try {
+        const result = await repo.query("SELECT * FROM product");
+        
+        console.table(result);
+    } catch (err) {
+        console.error(err);
+    }
+
+    try {
+        const result = await repo.retrieve("Betoniarka");
+        
+        result.forEach(r => {
+            console.log(`${r.id} | ${r.name} | ${r.price} zł | ${r.description} | ${r.quantity}`);
+        });
+    } catch (err) {
+        console.error(err);
+    }
+
+    // try {
+    //     const result = await repo.insert({
+    //         name: "Kremówka",
+    //         price: 21.37,
+    //         description: "xd",
+    //         quantity: 2137
+    //     });
+
+    //     console.log(result);
+    // } catch (err) {
+    //     console.error(err);
+    // }
+    
+    await repo.close();
+})();
