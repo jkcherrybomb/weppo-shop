348355a4b2b26a1a82ac9d9c135f4ebe8947d642
Julia Konefal <konefal.julia@gmail.com>
2024-01-11 22:14:17 +0100

    basic fronted file layout and navbar

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-const express = require('express');
-const routes = require('./routes/index');
+var http = require('http');
+var express = require('express');
+var app = express();
 
-const app = express();
-app.use('/', routes);
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-module.exports = app;
+app.get('/', (req, res) => {
+    res.render('index', {firstName: "Julia", lastName: "Konefal"});
+});
 
+app.get('/shopping_cart', (req, res) => {
+    res.render('shopping_cart');
+});
+
+app.get('/admin_page', (req, res) => {
+    res.render('admin_page');
+});
+
+app.get('/login_page', (req, res) => {
+    res.render('login_page');
+});
+
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+
+let username = "Julka";
+
+http.createServer(app).listen(3000);
