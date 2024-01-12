57f7299346975ad275330d574e7b4cebd818a79a
Julia Konefal <konefal.julia@gmail.com>
2024-01-12 18:01:23 +0100

    main page show products

--- app.js
+++ app.js
@@ -5,8 +5,38 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+fn = "Julia"
+ln = "Konefal"
+var a = [];
+a[0] = {
+    name: "bananas",
+    price: 2.50,
+    description:"bananas from brazil, price per kg",
+    quantity: 45
+}
+
+a[1] = {
+    name: "oreos",
+    price: 5.00,
+    description:"standard oreos",
+    quantity: 30
+}
+
+a[2] = {
+    name: "water Żywiec Zdrój",
+    price: 1.23,
+    description:"1l",
+    quantity: 25
+}
+a[3] = {
+    name: "orange juice Tymbark",
+    price: 2.50,
+    description:"1 liter",
+    quantity: 5
+}
+
 app.get('/', (req, res) => {
-    res.render('index', {firstName: "Julia", lastName: "Konefal"});
+    res.render('index', {firstName: fn, lastName: ln, products: a});
 });
 
 app.get('/shopping_cart', (req, res) => {
